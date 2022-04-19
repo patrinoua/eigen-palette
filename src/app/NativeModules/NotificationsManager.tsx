78cb46a16cb3a1b210fbf322a49733d5f326a63a
Angela Patrinou <patrinoua@gmail.com>
2022-04-20 00:03:02 +0200

    remove native modules, relay and scenes

--- src/app/NativeModules/NotificationsManager.tsx
+++
@@ -1,6 +0,0 @@
-import { NativeEventEmitter } from "react-native"
-import { LegacyNativeModules } from "./LegacyNativeModules"
-
-export const NotificationsManager = new NativeEventEmitter(
-  LegacyNativeModules.ARNotificationsManager as any
-)
