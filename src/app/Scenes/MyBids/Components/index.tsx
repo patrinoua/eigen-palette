40b1785e268ae9636ed1e2d9fd16610250774401
Angela Patrinou <patrinoua@gmail.com>
2022-04-20 00:33:22 +0200

    remove from Scenes...

--- src/app/Scenes/MyBids/Components/index.tsx
+++
@@ -1,4 +0,0 @@
-export { ActiveLotStandingFragmentContainer } from "./ActiveLotStanding"
-export { MyBidsPlaceholder } from "./MyBidsPlaceholder"
-export { ClosedLotStandingFragmentContainer } from "./ClosedLotStanding"
-export { SaleCardFragmentContainer } from "./SaleCard"
